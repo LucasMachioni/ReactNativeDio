1b396cb9085c4219be713681436d42f649780bc5
Lucas Machioni Jorge <lucas.jorge@grupoprovac.com.br>
2024-10-22 14:02:33 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+
+export default function App() {
+const [isActive, setIsActive] = useState (false)
+
+  function handleSymbol() {
+    setIsActive((oldValue: boolean) => {
+      return !oldValue
+    })
+  }
+
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+        <TouchableOpacity onPress={handleSymbol} >
+       <Image
+       source={isActive ? symbolOn : symbolOff}
+       />
+        </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerOn: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+{/**comentário*/}
